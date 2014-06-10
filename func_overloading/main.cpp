c7ac130bfd0001a892d81640cdb862b27eaad8da
susan <mwauragerald@njuguna-2.local>
2014-06-10 17:02:07 +0300

    Initial Commit

---
+++ func_overloading/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  func_overloading
+//
+//  Created by susan on 10/06/14.
+//  Copyright (c) 2014 nyumbani. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
