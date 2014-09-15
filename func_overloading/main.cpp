636fb7d89c42e38a4b8d3559371cc6b62929576c
sukumatech <mwauragerald@njuguna-2.local>
2014-09-15 19:23:45 +0300

    starting out

--- func_overloading/main.cpp
+++ func_overloading/main.cpp
@@ -8,11 +8,50 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[])
+using namespace std;
+
+void printNum(int x){
+    
+    cout << "this is an interger: " << x << endl;
+}
+void printNum(float x){
+    cout << "this is an interger: " << x << endl;
+}
+
+//recursion
+//remember base case in recurssive functions
+
+int factorial(int x){
+    if (x==1) {
+        return 1;
+    }else{
+        return x*factorial(x-1);
+    }
+}
+
+//pass array to function
+
+
+
+int main()
 {
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    int a =10;
+    float b =10.234;
+    
+    //arrays
+    int size = 5;
+    int value[size];
+    
+    for (int i =0; i<size; i++) {
+        value[i]=i;
+        cout << value[i] <<endl;
+    }
+    //pass array in to a function
+    
+    printNum(a);
+    printNum(b);
+    cout << factorial(5) << endl;
+    
 }
 
